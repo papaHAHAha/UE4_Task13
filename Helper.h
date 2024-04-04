6fa5e71ab716663d908ab74593b199c5d04ccf83
kirill <golovinkiruxa@gmail.com>
2024-04-04 16:44:51 +0100

    Add project files.

---
+++ Helper.h
@@ -0,0 +1,6 @@
+
+int sqrtSum(int a, int b)
+{
+	int res = a + b;
+	return res * res;
+}
---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Helper.h"
+
+
+int main()
+{
+	int result = sqrtSum(1, 2);
+	std::cout << "result = " << result << std::endl;
+}
