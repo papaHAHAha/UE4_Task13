e07235b290789cee6207f7f90827c4bb3c4e6906
kirill <golovinkiruxa@gmail.com>
2024-04-05 20:43:48 +0100

    task14

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include "Helper.h"
+#include <string>
 
 
 int main()
 {
+	//task_13
 	int result = sqrtSum(1, 2);
 	std::cout << "result = " << result << std::endl;
+
+	//task14
+	std::string name{ "Ivan" };
+	std::cout << name << "\n";
+	std::cout << name.length() << "\n";
+	std::cout << name[0] << "\n";
+	std::cout << name[3] << "\n";
 }
