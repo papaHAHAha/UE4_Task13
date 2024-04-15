6a6d75c7a1c06ffd085d1f028f07361cbc34389c
kirill <golovinkiruxa@gmail.com>
2024-04-15 02:38:23 +0100

    task16

--- Main.cpp
+++ Main.cpp
@@ -28,20 +28,22 @@ int main()
 {
 	//task_13
 	int result = sqrtSum(1, 2);
-	std::cout << "result = " << result << std::endl;
+	//std::cout << "result = " << result << std::endl;
 
-	//task14
+	/*
+	task14
 	std::string name{ "Ivan" };
 	std::cout << name << "\n";
 	std::cout << name.length() << "\n";
 	std::cout << name[0] << "\n";
 	std::cout << name[3] << "\n";
+	*/
 
 	//task15 too
-	std::cout << "even numbers from 0 to " << N << "\n";
+	//std::cout << "even numbers from 0 to " << N << "\n";
 	for (int i = 0; i <= N; i += 2)
 	{
-		std::cout << i << "\n";
+		//std::cout << i << "\n";
 	}
 	
 	/*if(false)
@@ -61,7 +63,34 @@ int main()
 		}
 	}*/
 
-	printNumbers(N, true);
+	/*printNumbers(N, true);
 	std::cout << "\n";
-	printNumbers(N, false);
+	printNumbers(N, false);*/
+
+	//task 16
+	const int N = 5;
+	int array[N][N];
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			array[i][j] = i + j;
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			std::cout.width(3);
+			std::cout << array[i][j];
+		}
+		std::cout << std::endl;
+	}
+	int currentDate = 15;
+	int index = currentDate % N;
+	int sum = 0;
+	
+	for (int j = 0; j < N; j++) {
+		sum += array[index][j];
+	}
+	std::cout << "sum of numbers in line " << index << " = " << sum << std::endl;
 }
